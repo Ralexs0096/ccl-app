e0e85df509d157f15d2fff10acf0229dea4ab97b
Alexs Ruiz <ralexs.acu@gmail.com>
2025-04-19 17:39:56 -0600

    Add forgot-password page view

--- app/routes/forgot-password.tsx
+++ app/routes/forgot-password.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import { Label } from '@radix-ui/react-label';
+import { Button } from '~/components/ui/button';
+import { Input } from '~/components/ui/input';
 
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  return (
+    <form className="flex flex-col gap-6">
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Reset your password</h1>
+        <p className="text-balance text-sm text-muted-foreground">
+          Enter your email address and we will send you a verification code
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            placeholder="your@email.com"
+            required
+          />
+        </div>
+        <Button type="submit" className="w-full">
+          Send reset instructions
+        </Button>
+        <div className="text-center text-sm">
+          Remember your password?{' '}
+          <a href="/auth/login" className="underline underline-offset-4">
+            Back to login
+          </a>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default ForgotPassword;
