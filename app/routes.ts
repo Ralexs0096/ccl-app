9b7442f168537ccbe89b9434dce7ca77421f22f1
Alexs Ruiz <ralexs.acu@gmail.com>
2025-04-21 23:22:29 -0600

    Add logout action and update routing to include logout functionality in auth layout

--- app/routes.ts
+++ app/routes.ts
@@ -10,7 +10,10 @@ export default [
   ...prefix('/auth', [
     layout('layouts/auth-layout.tsx', [
       route('login', 'routes/auth/login-page.tsx'),
-      route('forgot-password', 'routes/auth/forgot-password.tsx')
+      route('forgot-password', 'routes/auth/forgot-password.tsx'),
+
+      // Actions
+      route('logout', 'actions/logout.action.ts')
     ])
   ]),
   layout('layouts/main-layout.tsx', [index('routes/home-page.tsx')])
