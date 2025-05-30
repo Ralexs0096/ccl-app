70cd15b318170c15fd11c2b47bf994c195c850ea
Alexs Ruiz <ralexs.acu@gmail.com>
2025-05-29 20:58:53 -0600

    Add Session model and update User model to support nullable userId

--- app/prisma/migrations/20250530025045_add_session_model/migration.sql
+++ app/prisma/migrations/20250530025045_add_session_model/migration.sql
@@ -9,6 +9,3 @@ CREATE TABLE "Session" (
 
     CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
 );
-
--- AddForeignKey
-ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
